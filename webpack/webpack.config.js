136e7ecffb7ec215a9c35f051cfda6418721f789
LewisLen <csshack2016@126.com>
2021-03-07 22:31:07 +0800

    add: 压缩css

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -2,6 +2,7 @@ const Path = require("path");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin');
 module.exports = {
   entry: './src/index.js',
   output:{
@@ -107,6 +108,8 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: 'css/[name].[chunkhash:8].css',
     }),
+    // 压缩css
+    new OptimizeCssAssetsWebpackPlugin(),
     // 打包前先清空文件夹
     new CleanWebpackPlugin()
   ],
