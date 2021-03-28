1de05f593c61fddf0b7b921d5311d66673ec0b4c
LewisLen <csshack2016@126.com>
2021-03-28 11:11:03 +0800

    add:添加li鼠标跟随样式

--- React/React-cli/src/component/item/index.jsx
+++ React/React-cli/src/component/item/index.jsx
@@ -2,16 +2,27 @@ import React, { Component } from 'react';
 import './index.css';
 
 export default class Item extends Component {
+  state = {
+    mouseFlag: false
+  }
+  // 直接传值，用高阶函数回调
+  handleMouse = (flag) => {
+    return () => {
+      // console.log(flag)
+      this.setState({
+        mouseFlag:flag
+      })
+    }
+  }
   render() {
-    console.log(this.props)
     const {id,name,done} = this.props;
     return (
-        <li className="todo-item" key={id}>
+        <li className="todo-item" key={id} onMouseEnter={this.handleMouse(true)} onMouseLeave={this.handleMouse(false)} style={{backgroundColor: this.state.mouseFlag?"#ddd":"#fff"}}>
           <label>
             <input type="checkbox" defaultChecked={done}/>
             <span>{name}</span>
           </label>
-          <button>删除</button>
+          <button style={{display: this.state.mouseFlag?"block":"none"}}>删除</button>
         </li>
     )
   }
