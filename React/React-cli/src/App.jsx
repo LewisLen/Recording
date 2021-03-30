08873f266983fce381f1df0d0b42461f0ab7b818
LewisLen <csshack2016@126.com>
2021-03-30 18:06:42 +0800

    add:新增antdUI组件库

--- React/React-cli/src/App.jsx
+++ React/React-cli/src/App.jsx
@@ -1,6 +1,7 @@
 // 默认暴露和分别暴露
 import React, { Component } from 'react';
 import { Route, Switch } from "react-router-dom";
+import '../node_modules/antd/dist/antd.css';
 import './App.css';
 import Nav from './views/Nav';
 import Search from './views/Search';
--- React/React-cli/src/views/Nav.jsx
+++ React/React-cli/src/views/Nav.jsx
@@ -1,19 +1,46 @@
+import {
+  DesktopOutlined,
+  MenuFoldOutlined, MenuUnfoldOutlined,
+  PieChartOutlined
+} from '@ant-design/icons';
+import { Button, Menu } from 'antd';
 import React, { Component } from 'react';
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
+
+// const { SubMenu } = Menu;
 
 export default class Nav extends Component {
+  state = {
+    collapsed: false,
+  };
+
+  toggleCollapsed = () => {
+    this.setState({
+      collapsed: !this.state.collapsed,
+    });
+  };
+
   render() {
     return (
-      <div>
-        <ul>
-          <li>
+      <div style={{ width: 256 }}>
+        <Button type="primary" onClick={this.toggleCollapsed} style={{ marginBottom: 16 }}>
+          {React.createElement(this.state.collapsed ? MenuUnfoldOutlined : MenuFoldOutlined)}
+        </Button>
+        <Menu
+          defaultSelectedKeys={['1']}
+          defaultOpenKeys={['sub1']}
+          mode="inline"
+          theme="dark"
+          inlineCollapsed={this.state.collapsed}
+        >
+          <Menu.Item key="todo" icon={<PieChartOutlined />}>
             <Link to="/todo">Todo</Link>
-          </li>
-          <li>
+          </Menu.Item>
+          <Menu.Item key="search" icon={<DesktopOutlined />}>
             <Link to="/search">Search</Link>
-          </li>
-        </ul>
+          </Menu.Item>
+        </Menu>
       </div>
-    )
+    );
   }
 }
